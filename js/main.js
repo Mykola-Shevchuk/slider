66d1739c538cd4693468bb8a5cd949700e670d4a
Mykola Shevchuk <mykola.shevchuk.ua@gmail.com>
2016-06-14 03:41:17 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,39 +1,42 @@
-	// MouseOver
-
-	document.onmouseover = function(event) {
-	  var target = event.target,
-	      fieldCoords = target.getBoundingClientRect();
-	  
-	  var fieldInnerCoords = {
-    	top: fieldCoords.top + pageYOffset,
-    	left: fieldCoords.left + pageXOffset
-  	  };
-
-  	  var atr = target.getAttribute('data-tooltip');
-	  var hoverBlock = document.getElementById('hovBlock');
-
-	  if(!atr) return;
-	 
-	  hoverBlock.style.display = "block";
-	  hoverBlock.innerHTML = atr;
-
-	  var left = fieldInnerCoords.left + ( (target.offsetWidth - hoverBlock.offsetWidth) / 2 );
-	  if(left < 0) left = 0;
-
-	  var top = fieldInnerCoords.top - 35;
-	  if(fieldCoords.top < 20) {
-	  	top = fieldInnerCoords.top + target.offsetHeight + 5;
-	  };
-
-	  hoverBlock.style.top = top + "px";
-      hoverBlock.style.left = left + "px";
-
-	  this.onmouseout = function() {
-	  	hoverBlock.style.display = "none";
-	  }
-
-	}
-	
-
 
 	
+
+//Slider
+
+  /* этот код помечает картинки, для удобства разработки */
+    var lis = document.getElementsByTagName('li');
+    for (var i = 0; i < lis.length; i++) {
+      lis[i].style.position = 'relative';
+      var span = document.createElement('span');
+      // обычно лучше использовать CSS-классы,
+      // но этот код - для удобства разработки, так что не будем трогать стили
+      span.style.cssText = 'position:absolute;left:0;top:0';
+      span.innerHTML = i + 1;
+      lis[i].appendChild(span);
+    }
+
+    /* конфигурация */
+    var width = 130; // ширина изображения
+    var count = 3; // количество изображений
+
+    var carousel = document.getElementById('carousel');
+    var list = carousel.querySelector('ul');
+    var listElems = carousel.querySelectorAll('li');
+
+    var position = 0; // текущий сдвиг влево
+
+    carousel.querySelector('.prev').onclick = function() {
+      // сдвиг влево
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position + width * count, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      // сдвиг вправо
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position - width * count, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+
+
